2930f1233b666c3182cc0210e5b0f52ef3042ede
Daykon98 <miguel.angel.vk@hotmail.com>
2019-12-01 20:37:07 +0100

    Claridad y seaborn
    Utilizacion de seaborn en lugar de plt.plot, guarda los resultados de forma mas organizada, ahora guarda los modelos, y el codigo está mas limpio (quitando código viejo) y mejor comentado

--- spark/kmeans_classification.py
+++ spark/kmeans_classification.py
@@ -4,11 +4,10 @@ from pyspark.sql.types import DoubleType
 from pyspark.sql.functions import udf
 from pyspark.ml.clustering import KMeans
 from pyspark.ml import Pipeline
-from pyspark.ml.feature import VectorAssembler, PCA, MinMaxScaler
+from pyspark.ml.feature import VectorAssembler, PCA, Normalizer, StandardScaler
+
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-from pyspark.ml.feature import Normalizer, StandardScaler #Quedarnos con el que nos mole
-from matplotlib import animation
+import seaborn as sns; sns.set()
 import pandas as pd
 import prepro
 import string
@@ -16,12 +15,10 @@ import string
 import time
 
 #######################################################################################################
-#LEE ESTO PORFA
-
-#ESto para matplotlib animator apt-get install ffmpeg
-#Necesita instalar pandas
+#sudo pip3 install seaborn
+#sudo pip3 install pandas
 #Necesita instalar tkinter #sudo apt-get install python3-tk
-#Asegurarse que se utiliza python3.6 porfavor #PON EN CONSOLA export PYSPARK_PYTHON=/usr/bin/python3.6
+#Asegurate de utilizar python3.6 #PON EN CONSOLA export PYSPARK_PYTHON=/usr/bin/python3.6
 #######################################################################################################
 
 #Configuramos spark, creamos el sparkContext y la sesion
@@ -53,27 +50,17 @@ pre_temp = ['PRESSURE','AIR_TEMPERATURE']
 indep = ['MINIMUM_X_AXIS','MEAN_X_AXIS_CROSSINGS',
        'MEAN_Y_AXIS_CROSSINGS', 'MEAN_Z_AXIS_CROSSINGS','WIND_DIRECTION']
 
-assembler = VectorAssembler(
-    inputCols=sisms,
-    outputCol='sisms')
-
-#agregamos la columna
+#Creamos nuevas columnas con los grupos
+assembler = VectorAssembler(inputCols=sisms, outputCol='sisms')
 df = assembler.transform(df)
 
-assembler = VectorAssembler(
-    inputCols=pre_temp,
-    outputCol='pre_temp')
-
-#agregamos la columna
+assembler = VectorAssembler(inputCols=pre_temp, outputCol='pre_temp')
 df = assembler.transform(df)
 
-assembler = VectorAssembler(
-    inputCols=indep,
-    outputCol='indep')
-
-#agregamos la columna
+assembler = VectorAssembler(inputCols=indep, outputCol='indep')
 df = assembler.transform(df)
 
+#Vamos a crear nuevas columnas con los datps estrandarizados con 
 scaler = StandardScaler(inputCol="sisms", outputCol="sismsNorm")
 scalerModel = scaler.fit(df)
 df = scalerModel.transform(df)
@@ -106,84 +93,69 @@ assembler = VectorAssembler(
 #agregamos la columna
 trainingData = assembler.transform(df)
 
+#Creamos un par de arrays que guarden el coste de cada modelo para poder
+#represemtarñp gráficamente luego
 computingCostIndex = []
 computingCost = []
 
+#Creo un diccionario con colores para representar graficamente luego los resultados
+color_dict = dict({0:'tomato',
+                  1:'limegreen',
+                  2: 'orange',
+                  3: 'saddlebrown',
+                  4: 'dodgerblue',
+                  5: 'paleturquoise',
+                  6: 'orchid',
+                  7: 'grey',
+                  8: 'lightpink'})
+
 log = open("log.txt", "a+")
-for i in range(4, 5):
+#Bucle para probar con distinto número de grupos
+for i in range(3, 9):
 	#start_time = time.time()
+	#Entrenamos el modelo de Kmeans con un numero de grupos igual a i
 	kmeans = KMeans().setK(i)
 	model = kmeans.fit(trainingData)
 
-	#Mostramos la info del resultado, copiado y pegado de la pagina de documentacion
+	#Computamos el cosete del modelo
 	wssse = model.computeCost(trainingData)
-	print("Within Set Sum of Squared Errors = " + str(wssse))
 
 	#guardado del modelo
-	#model.save('modelo_kmeans_' + str(i) + '.model')
+	model.save('model/KM' + str(i) + '.model')
 
+	#Aniadimos el coste a los arrays
 	computingCostIndex.append(i)
 	computingCost.append(wssse)
 
-	# Shows the result.
-	centers = model.clusterCenters()
-	print("Cluster Centers: ")
-	for center in centers:
-	    print(center)
-
+	#Aniadimos la prediccion con el modelo entrenado
+	#anteriormente a nuestros datos
 	transformed = model.transform(trainingData)
 
-	#countTable = transformed.groupBy("prediction").agg({"prediction":"count"})
+	#Creamos archivos con los datos estadisticos de cada grupo 
+	#para su posterior analisis
+	describe = transformed
+	for j in range (i):
+		describeGrouped = describe.filter(describe.prediction == j)
+		describeGrouped = describeGrouped.describe()
+		describeGrouped.write.option("header", "true").csv("describe/describe_kmeans_" + str(i) + "/precition_" + str(j) + ".txt", mode="overwrite")
 
-	#transformed = transformed.groupBy('prediction').avg(*features)
+	#Creamos varios graficos que ilustren los grupos formados
+	plt.figure()
+	sns_fig = sns.scatterplot(x=pdDF['RMS_X_AXIS_X100'], y=pdDF['SEISMIC_TIME_SOLS'], hue=pdDF['prediction'], linewidth=0, alpha = 0.7, palette=color_dict, legend=False)
+	plt.savefig("images/KM" + str(i) + "_rms_sols.png")
 
-	#transformed.write.option("header", "true").json("prueba.out")
-	#print("--- %i: %s seconds ---\n" % (i, (time.time() - start_time)))
-	#log.write("--- %i: %s seconds ---\n" % (i, (time.time() - start_time)))
+	plt.figure()
+	sns_fig = sns.scatterplot(x=pdDF['RMS_X_AXIS_X100'], y=pdDF['PRESSURE'], hue=pdDF['prediction'], linewidth=0, alpha = 0.7, palette=color_dict, legend=False)
+	plt.savefig("images/KM" + str(i) + "_rms_pres.png")
 
-	#figure = plt.figure()
-	#ax = Axes3D(figure)
-	pdDF = transformed.toPandas()
+	plt.figure()
+	sns_fig = sns.scatterplot(x=pdDF['RMS_X_AXIS_X100'], y=pdDF['WIND_DIRECTION'], hue=pdDF['prediction'], linewidth=0, alpha = 0.7, palette=color_dict, legend=False)
+	plt.savefig("images/KM" + str(i) + "_rms_winddir.png")	
 
-	######################################################################################################
-	'''
-	print("ANIMATING...")
-	def init():
-		ax.scatter(pdDF['MAXIMUM_X_AXIS'], pdDF['WINDSPEED'], pdDF['PRESSURE'], c=pdDF['prediction'])
-		ax.set_xlabel('MAXIMUM_X_AXIS')
-		ax.set_ylabel('WINDSPEED')
-		ax.set_zlabel('PRESSURE')
-		return figure,
-
-	def animate(i): 
-		ax.view_init(elev=10., azim=i)
-		return figure,
-
-	# Animate
-
-	anim = animation.FuncAnimation(figure, animate, init_func=init,frames=360, interval=20, blit=True)
-	# Save
-	anim.save('clustering.mp4', fps=30, extra_args=['-vcodec', 'libx264'],  progress_callback= lambda x, i: print(f'Saving frame {x}/{i}')) #progress_callback requires matplotlib 3.1.1
-	print("Saved")
-	'''
-	######################################################################################################
-
-	plt.scatter(pdDF['RMS_X_AXIS_X100'], pdDF['SEISMIC_TIME_SOLS'], c = pdDF['prediction'], s = 10)
-	plt.xlabel("RMS_X_AXIS_X100")
-	plt.ylabel("SEISMIC_TIME_SOLS")
-
-	'''plot = ax.scatter(pdDF['WINDSPEED'].str[0], pdDF['AIR'].str[1], pdDF['features'].str[2], c = pdDF['prediction'])
-	ax.set_xlabel('windspeed')
-	ax.set_ylabel('air_temp')
-	ax.set_zlabel('RMS_X_AXIS_X100')'''
-
-	#fig = plt.get_figure()
-	plt.savefig("output_kmeans_nice" + str(i) + ".png")
-
-
+#Mostramos el gráfico con el coste
 plt.figure()
 plt.plot(computingCostIndex, computingCost, '-')
 plt.ylabel('Within set sum of squared errors')
 plt.xlabel('Number of clusters')
-plt.savefig("codo.png")
+plt.savefig("codo2.png")
 
